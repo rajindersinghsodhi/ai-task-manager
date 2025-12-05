a580f4c2843f0397d3982b98f2bbf1b8b595a8d6
rajindersinghsodhi <rajsodhisocial@gmail.com>
2025-12-05 18:16:28 +0530

    config(env): added allowed origin variable in env file

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 import express from "express";
 import cors from "cors";
 import { taskRoutes } from "./routes/task.js";
+import { ALLOWED_ORIGINS } from "./config/env.js";
 
 const app = express();
 
 app.use(cors({
-  origin: ["http://localhost:3000"]
+  origin: ALLOWED_ORIGINS
 }))
 app.use(express.json());
 
